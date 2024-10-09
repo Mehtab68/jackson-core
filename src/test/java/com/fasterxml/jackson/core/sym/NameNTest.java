6dfc0edde47d0bab3cf9e92e977e06ef3aff9846
Tibby <haoran.sun@umontreal.ca>
2024-10-09 17:13:06 -0400

    Update NameNTest.java

--- src/test/java/com/fasterxml/jackson/core/sym/NameNTest.java
+++ src/test/java/com/fasterxml/jackson/core/sym/NameNTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.github.javafaker.Faker;
+import com.github.javafaker.Faker; 
 
 /**
  * Classe de test pour la classe {@link NameN}.
